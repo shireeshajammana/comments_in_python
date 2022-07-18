9cc3f10d79763c03be5690babff645af6449b22e
shireeshajammana <109510692+shireeshajammana@users.noreply.github.com>
2022-07-18 22:14:45 +0530

    Add files via upload

---
+++ comments in  python.py
@@ -0,0 +1,9 @@
+'''
+Name: Siri
+Name & Day:18th July 2022(monday)
+File Desc: Types of comments in python
+            1)sngl Line comment
+            2)multi Line comment
+'''            
+#its a single line comment
+print('hey today we have seen comments in python')
